1ccb9bb58cc794c8e8dcb65b4fd496792cccd020
左二东 <left2east@hotmail.com>
2016-08-03 00:16:03 +0800

    wcf service

---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Person
+    {
+        public string name;
+        public string id;
+        public int age;
+    }
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public Person GetPerson(string id)
+        {
+            return new Person { name = "test", id = id, age = 12 };
+        }
+
+        public List<Person> GetPersons(string[] ids)
+        {
+            List<Person> list = new List<Person>();
+            foreach(string id in ids)
+            {
+                list.Add( new Person { name = "test", id = id, age = 12 });
+            }
+            return list;
+        }
+
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
